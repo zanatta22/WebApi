95e02406d4323035f20c6a6870fff3f70a234a14
Guilherme Zanatta <Guilherme.Zanatta@ectp.com>
2020-06-04 14:37:12 -0300

    Commit teste

--- WebApi/Controllers/PaymentDetailsController.cs
+++ WebApi/Controllers/PaymentDetailsController.cs
@@ -43,8 +43,6 @@ namespace WebApi.Controllers
 
 
         // PUT: api/PaymentDetails/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetail paymentDetail)
         {
