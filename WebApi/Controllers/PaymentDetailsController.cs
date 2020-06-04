3238e87971e01387ce08b8dd4f4cd00caf771f5a
Guilherme Zanatta <Guilherme.Zanatta@ectp.com>
2020-06-04 14:35:16 -0300

    Primeiro Commit.

--- WebApi/Controllers/PaymentDetailsController.cs
+++ WebApi/Controllers/PaymentDetailsController.cs
@@ -75,8 +75,6 @@ namespace WebApi.Controllers
         }
 
         // POST: api/PaymentDetails
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
         {
